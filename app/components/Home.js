7aec2e8759e165c8b68cdbcb166a15f90b8adaf3
shawjia <shawjia@me.com>
2018-09-22 12:27:38 +0800

    🍱 add antd

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,19 +1,12 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
-import styles from './Home.css';
+import { Spin } from 'antd';
 
-type Props = {};
-
-export default class Home extends Component<Props> {
-  props: Props;
-
+export default class Home extends Component {
   render() {
     return (
-      <div className={styles.container} data-tid="container">
-        <h2>Home</h2>
-        <Link to={routes.COUNTER}>to Counter</Link>
+      <div>
+        <Spin size="large" />
       </div>
     );
   }
