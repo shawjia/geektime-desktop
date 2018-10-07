95b00771d3264b3e194712a7fae35db3e07925ff
shawjia <shawjia@me.com>
2018-10-07 12:10:06 +0800

    ✨ new feature: search

--- app/components/Articles.js
+++ app/components/Articles.js
@@ -1,33 +1,57 @@
-import React, { Component } from 'react';
-import { List, Avatar } from 'antd';
+import React, { Component, Fragment } from 'react';
+import { List, Avatar, Input, message } from 'antd';
 import { connect } from 'react-redux';
 
+const { Search } = Input;
+
 class Articles extends Component {
+
+  state = {
+    search: '',
+  }
+
+  onSearch = (search) => {
+    this.setState({ search });
+  }
 
   render() {
 
     const { articles, fetchArticle, aid } = this.props;
+    const { search } = this.state;
+    const filterArticles = search === ''
+      ? articles
+      : articles.filter(v => v.article_title.includes(search));
 
-    if (articles.length === 0) {
-      return null;
+    if (articles.length && (filterArticles.length === 0)) {
+      message.info(`找不到匹配${search}的文章`);
     }
 
     return (
-      <List
-        itemLayout="horizontal"
-        dataSource={articles}
-        renderItem={item => (
-          <List.Item onClick={() => fetchArticle(item.id)}
-            style={{ background: aid === item.id ? 'white' : '#fafafa' }}
-          >
-            <List.Item.Meta
-              avatar={<Avatar src={item.article_cover} />}
-              title={item.article_title}
-              style={{paddingLeft: 10, paddingRight: 10 }}
-            />
-          </List.Item>
-        )}
-      />
+      <Fragment>
+         <Search
+          placeholder="搜索标题"
+          onSearch={this.onSearch}
+          style={{ width: '100%', padding: '5px' }}
+        />
+
+        {filterArticles.length > 0 &&
+          <List
+            itemLayout="horizontal"
+            dataSource={filterArticles}
+            renderItem={item => (
+              <List.Item onClick={() => fetchArticle(item.id)}
+              style={{ background: aid === item.id ? 'white' : '#fafafa' }}
+              >
+                <List.Item.Meta
+                  avatar={<Avatar src={item.article_cover} />}
+                  title={item.article_title}
+                  style={{paddingLeft: 10, paddingRight: 10 }}
+                  />
+              </List.Item>
+            )}
+          />
+        }
+      </Fragment>
     )
   }
 };
